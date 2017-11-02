849815baf974a45474076c1b152db036ee5fd31e
Iman Tabrizian <tabrizian@outlook.com>
2017-11-02 15:55:16 -0400

    Add my onos applications

--- foo-app/src/main/java/org/foo/app/AppCommand.java
+++ foo-app/src/main/java/org/foo/app/AppCommand.java
@@ -13,31 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package me.imantabrizian;
+package org.foo.app;
 
-import org.apache.felix.scr.annotations.Activate;
-import org.apache.felix.scr.annotations.Component;
-import org.apache.felix.scr.annotations.Deactivate;
-import org.apache.felix.scr.annotations.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.karaf.shell.commands.Command;
+import org.onosproject.cli.AbstractShellCommand;
 
 /**
- * Skeletal ONOS application component.
+ * Sample Apache Karaf CLI command
  */
-@Component(immediate = true)
-public class AppComponent {
+@Command(scope = "onos", name = "sample",
+         description = "Sample Apache Karaf CLI command")
+public class AppCommand extends AbstractShellCommand {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    @Activate
-    protected void activate() {
-        log.info("Started");
-    }
-
-    @Deactivate
-    protected void deactivate() {
-        log.info("Stopped");
+    @Override
+    protected void execute() {
+        print("Hello %s", "World");
     }
 
 }
